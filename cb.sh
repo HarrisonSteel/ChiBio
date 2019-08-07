be79d114a584561550af7a262dd9a0c96796620e
HarrisonSteel <53864271+HarrisonSteel@users.noreply.github.com>
2019-08-07 13:43:35 +0100

    Add files via upload

---
+++ cb.sh
@@ -0,0 +1,1 @@
+gunicorn -b 192.168.7.2:5000 app:application
---
+++ setup.sh
@@ -0,0 +1,31 @@
+cd ~/../etc/ssh/
+echo "PermitRootLogin yes" >> sshd_config
+echo -e "root\nroot" | passwd root
+sed -i 's@-w /var/lib/cloud9@-w /root/chibio@' /lib/systemd/system/cloud9.service
+sed -i 's@1000@root@' /lib/systemd/system/cloud9.service
+cd ~/../etc/
+echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" >> resolv.conf
+sudo /sbin/route add default gw 192.168.7.1
+sudo apt-get update
+sudo apt-get --assume-yes install unzip
+mkdir ~/../root/chibio
+cd ~/../home/debian
+unzip chibio1.zip -d ~/../root/chibio
+sudo apt --assume-yes install python-pip
+pip install Gunicorn
+pip install flask
+pip install serial
+pip install Adafruit_GPIO
+pip install --user --upgrade setuptools
+pip install simplejson
+pip install smbus2
+pip install numpy
+cd ~/../tmp
+pip download Adafruit_BBIO
+tar xvzf Adafruit_BBIO-1.1.1.tar.gz
+cd Adafruit_BBIO-1.1.1/
+sed -i "s/'-Werror', //g" setup.py
+python setup.py install
+cd ~/../root/chibio
+chmod +x cb.sh
+reboot now
