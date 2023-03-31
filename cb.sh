0ba605e0952eea399a58ecae48297a64c0713c9d
Marco Galardini <marco.galardini@twincore.de>
2023-03-31 10:47:53 +0200

    Leave the default IP address

--- cb.sh
+++ cb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Uncomment the following line to run ChiBio in the background
-# screen -dmS ChiBio bash -c "gunicorn -b 192.168.178.116:5000 app:application"
+# screen -dmS ChiBio bash -c "gunicorn -b 192.168.7.2:5000 app:application"
 # Then, comment out the next line
 cd /root/chibio
-gunicorn -b 192.168.178.116:5000 app:application --capture-output --log-level info --timeout 0
+gunicorn -b 192.168.7.2:5000 app:application --capture-output --log-level info --timeout 0
 
